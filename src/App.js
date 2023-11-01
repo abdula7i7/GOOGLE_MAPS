a10f43e384847f15e5f0510cd6afda9f852ee9d7
abdulaziz <8enior@gmail.com>
2023-11-01 14:59:49 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./pages";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home/>
     </div>
   );
+
 }
 
+
+
 export default App;
+
+
+// import React, { Component } from "react";
+
+// export default class App extends Component {
+
+//   test() {
+//     console.log('hello gandon');
+//   }
+
+//   render() {
+//     return <h1 onClick={this.test} style={{ color: 'red', cursor: 'pointer' }}>
+//       salom
+//     </h1>
+//   }
+
+// }
+
+
